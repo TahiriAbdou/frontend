70dbaf3b1e1afec2d534e07ae96d85a1decc62cb
Luigi Poole <luigipoole@outlook.com>
2015-10-13 09:56:37 -0700

    get sidebar style corrected

--- src/components/Sidebar.react.js
+++ src/components/Sidebar.react.js
@@ -9,36 +9,34 @@ var Sidebar = React.createClass({
   },
   render: function () {
     return (
-
-
-
-      <ul className="sidebar">
+      <div className="sidebar">
+        <div className="nav-sidebar">
         <div className="nav-profile">
           <div className="nav-profile-picture pull-left">
             <img id="sideBarAvatar" src="images/avatar.png" className="img-circle profile-picture"/>
           </div>
           <div className="nav-username pull-left">AurousUser</div>
           <div className="nav-settings pull-right">
+          <Router.Link to="preferences">
             <button type="button" className="btn btn-transparent" data-toggle="modal" data-target="#modalSettings"><i className="material-icons">settings</i></button>
+          </Router.Link>
           </div>
         </div>
-
+        <ul className="nav nav-menu">
         <Router.Link to="dashboard">
-          <li>
-            <p>Dashboard</p>
-          </li>
+                <li id="nav-discover">
+                    <a><i className="material-icons">search</i><span>Discover</span>
+                    </a>
+                </li>
         </Router.Link>
-        <Router.Link to="preferences">
-          <li>
-            <p>Preferences</p>
-          </li>
-        </Router.Link>
-        <Router.Link to="about">
-          <li>
-            <p>About</p>
-          </li>
-        </Router.Link>
-      </ul>
+                <li id="nav-collection">
+                    <a ><i className="material-icons">queue_music</i><span>My Collection</span>
+                    </a>
+                </li>
+        </ul>
+        
+      </div>
+        </div>
     );
   }
 });
