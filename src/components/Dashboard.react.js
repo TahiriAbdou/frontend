3e7f8597cbf7d48be5df886260e56c16612fa9b6
Luigi Poole <luigipoole@outlook.com>
2015-10-13 12:07:28 -0700

    hide featured on search text

--- src/components/Dashboard.react.js
+++ src/components/Dashboard.react.js
@@ -21,28 +21,30 @@ var Dashboard = React.createClass({
 
     getInitialState: function() {
         return {
-           
+           searchQuery: false
         };
     },
-    
+    handleSearchChange: function(event) {
+        var value = (event.target.value !== '') ? event.target.value : false;
+        this.setState({
+            searchQuery: value
+        });
+    },
     render: function() {
         return (
-     <div id="discover">
-                            <form id="search-form">
-                                <div className="input-group">
-                                    <input id="searchbar" type="text" className="form-control" placeholder="Search for song or import URL"/>
-                                            <span className="input-group-btn">
-                                                <button className="btn btn-primary" type="button" id="btn-search"><i className="fa fa-search"></i></button>
-                                            </span>
-                                </div>
-                            </form>
-<Featured />
-
-
-
-
-
-     </div>
+            <div id="discover">
+                <form id="search-form">
+                    <div className="input-group">
+                        <input id="searchbar" type="text" className="form-control" onChange={this.handleSearchChange} placeholder="Search for song or import URL"/>
+                            <span className="input-group-btn">
+                                <button className="btn btn-primary" type="button" id="btn-search"><i className="fa fa-search"></i></button>
+                            </span>
+                    </div>
+                </form>
+                <If test={!this.state.searchQuery}>
+                    <Featured />
+                </If>
+            </div>
         );
 
     }
