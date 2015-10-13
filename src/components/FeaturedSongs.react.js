6d1787110b6fd14b7fadef7662ba7112270a2817
Luigi Poole <luigipoole@outlook.com>
2015-10-13 10:59:43 -0700

    clean up featured code

--- src/components/FeaturedSongs.react.js
+++ src/components/FeaturedSongs.react.js
@@ -8,74 +8,66 @@ import ReactDataGrid from 'react-data-grid';
 //generate a fixed number of rows and set their properties
 var _rows = [];
 for (var i = 1; i < 100; i++) {
-  _rows.push({
-    image: 'https://i1.sndcdn.com//artworks-000115464710-6br5gk-t500x500.jpg',
-    controls: 'Task ',
-    song: 'something',
-    artist : 'some artist',
-    album : 'some album',
-    length: '4:01'
-  });
+    _rows.push({
+        image: 'https://i1.sndcdn.com//artworks-000115464710-6br5gk-t500x500.jpg',
+        controls: 'Task ',
+        song: 'something',
+        artist: 'some artist',
+        album: 'some album',
+        length: '4:01'
+    });
 }
 
 
 //function to retrieve a row for a given index
-var rowGetter = function(i){
-  return _rows[i];
+var rowGetter = function(i) {
+    return _rows[i];
 };
 
 //Custom Formatter component
 var ImageFormatter = React.createClass({
-  render:function(){
-    return (
-     <img src={this.props.value} />);
+    render: function() {
+        return (<img src={this.props.value} />);
     }
-  });
+});
 
 var ControlsFormatter = React.createClass({
-  render:function(){
-    return (
-     <div>
- <button className="btn btn-transparent control-show"><a ><i className="material-icons">add</i></a></button>
-<button className="btn btn-transparent control-show"><a data-jq-dropdown="#dropdown"><i className="material-icons">more_horiz</i></a></button>
-<button className="btn btn-transparent"><a ><i id="discover-row-icon-0" className="material-icons">play_arrow</i></a></button>
-</div>
-
-     );
+    render: function() {
+        return (
+            <div>
+                <button className="btn btn-transparent control-show"><a ><i className="material-icons">add</i></a></button>
+                <button className="btn btn-transparent control-show"><a data-jq-dropdown="#dropdown"><i className="material-icons">more_horiz</i></a></button>
+                <button className="btn btn-transparent"><a ><i id="discover-row-icon-0" className="material-icons">play_arrow</i></a></button>
+            </div>
+        );
     }
-  });
+});
 
 
 //Columns definition
-var columns = [
-{
-  key: 'image',
-  name: 'image',
-  width: 75,
-  formatter : ImageFormatter
-},
-{
-  key: 'controls',
-  name: 'controls',
-  width: 120,
-  formatter : ControlsFormatter
-},
-{
-  key: 'song',
-  name: 'Song'
-},
-{
-  key: 'artist',
-  name: 'Artist'
-},
-{
-  key: 'album',
-  name: 'Album'
-},
-{
-  key: 'length',
-  name: 'length'
-},
+var columns = [{
+        key: 'image',
+        name: 'image',
+        width: 75,
+        formatter: ImageFormatter
+    }, {
+        key: 'controls',
+        name: 'controls',
+        width: 120,
+        formatter: ControlsFormatter
+    }, {
+        key: 'song',
+        name: 'Song'
+    }, {
+        key: 'artist',
+        name: 'Artist'
+    }, {
+        key: 'album',
+        name: 'Album'
+    }, {
+        key: 'length',
+        name: 'length'
+    },
 
 ];
 
@@ -98,31 +90,22 @@ var Featured = React.createClass({
 
     getInitialState: function() {
         return {
-           
+
         };
     },
-    
+
     render: function() {
-        var fistThStyle = {
-            width: '75px'
-        };
-         var SecondThStyle = {
-            width: '120p'
-        }
-
         return (
-        <div id="discoverIntro">
-                                <h2>Featured Songs</h2>
-                                <div className="result" id="top-result">
-<ReactDataGrid
-    columns={columns}
-    rowGetter={rowGetter}
-    rowsCount={_rows.length}
-    minHeight={500} />
-                                   
-                                </div>
-
-                            </div>
+            <div id="discoverIntro">
+                <h2>Featured Songs</h2>
+                <div className="result" id="top-result">
+                    <ReactDataGrid
+                    columns={columns}
+                    rowGetter={rowGetter}
+                    rowsCount={_rows.length}
+                    minHeight={500} />             
+                </div>
+            </div>
         );
 
     }
@@ -131,6 +114,3 @@ var Featured = React.createClass({
 
 
 module.exports = Featured;
-
-
-
